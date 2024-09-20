247be1aaaa022794ed16a63848a42cf05e6f9edb
kareemabdelgawad660@gmail.com <kareemabdelgawad660>
2024-09-20 14:37:51 +0300

    all the codes we used

---
+++ kareem.sh
@@ -0,0 +1,244 @@
+    1  man sudo_root
+    2  pwd
+    3  mkdir kareem
+    4  cd kareem
+    5  rmdir kareem
+    6  mkdir kareem
+    7  mkdir biocomputing && cd biocomputing
+    8  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+    9  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+   10  mv wildtype.fna ../kareem/
+   11  rm wildtype.gbk
+   12  grep "tatatata" ../kareem/wildtype.fna 
+   13  grep "tatatata" ../kareem/wildtype.fna > mutant_sequence
+   14  cat mutant_sequence 
+   15  grep "atg" mutant_sequence 
+   16  grep "atg" mutant_sequence > selcted_genes,text
+   17  cat selcted_genes,text 
+   18  grep "atg" selcted_genes,text 
+   19  grep "atgtggttgtag" selcted_genes,text 
+   20  grep "atgtggttgtag" selcted_genes,text > specific_gene . text 
+   21  grep "atgtggttgtag" selcted_genes,text > specific_gene
+   22  rm specific_gene 
+   23  grep "[200~atgtgatagaaacgt
+   24  pwd
+   25  grep "[200~atgtgatagaaacgt
+   26  grep "atgtgatagaaacgt
+   27  a" selcted_genes,text 
+   28  grep -o "atgtgatagaaacgt
+   29  a"
+   30  grep -o "atgtgatagaaacgt
+   31  a" selcted_genes,text 
+   32  cat selcted_genes,text 
+   33  grep "atg" selcted_genes,text 
+   34  grep -o  " atgtgacaattatggtgtag" selcted_genes,text 
+   35  pwd
+   36  cd biocomputing
+   37  ../biocomputing/
+   38  mv ../biocomputing/
+   39  mv --help
+   40  cd home
+   41  ls
+   42  less
+   43  lss
+   44  cd ..
+   45  lsblk
+   46  cp -r /home/kareem/biocomputing /media/kareem/Kareem/
+   47  mkdir kareem_abdelgawad
+   48  mkdir biocomputing && ../biocomputing
+   49  rmdir biocomputing
+   50  rmdir -r biocomputing/
+   51  rmdir -r /home/kareem/biocomputing/
+   52  rm -r /home/kareem/biocomputing/
+   53  mkdir kareem_abdelgawad
+   54  mkdir biocomputing && ../biocomputing
+   55  cd biocomputing
+   56  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+   57  mkdir test
+   58  wget https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=41296&extrafeat=null&conwithfeat=on&hide-cdd=on&ncbi_phid=CE8B8A826E9D238100000000069B05D2
+   59  https://www.ncbi.nlm.nih.gov/nuccore/X01714.1?report=fasta
+   60  wget https://www.ncbi.nlm.nih.gov/nuccore/X01714.1?report=fasta
+   61  wget https://www.ncbi.nlm.nih.gov/portal/utils/pageresolver.fcgi?recordid=66ea991228653f4d2484a35a
+   62  wget https://www.ncbi.nlm.nih.gov/nuccore/OR255918.1?report=fasta#
+   63  cg
+   64  cg_count=$(grep -o "[CG]" sequence.fasta | wc -l 
+   65  total_count=$(grep -o "[ATCG]" sequence.fasta | wc -l
+   66  cg_count=$(grep -o "[CG]" sequence.fasta | wc -l)
+   67  CD biocomputing/
+   68  cd biocomputing/
+   69  cg_count=$(grep -o "[CG]" sequence.fasta | wc -l)
+   70  cg_count=$(grep -o "[ATCG]" sequence.fasta | wc -l)
+   71  gc_percentage=$(echo "
+   72  mkdir kareem_abdelgawad
+   73  mkdir biocomputing && cd biocomputing 
+   74  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna 
+   75  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk 
+   76  mv wildtype.fna ../kareem_abdelgawad/
+   77  rm wildtype.gbk
+   78  grep "tatatata" ../kareem_abdelgawad/wildtype.fna 
+   79  grep "tatatata" ../kareem_abdelgawad/wildtype.fna > mutant_sequence.text 
+   80  wget https://www.ncbi.nlm.nih.gov/nuccore/X01714.1?report=fasta
+   81  wget https://www.ncbi.nlm.nih.gov/nuccore/X01714.1?report=fasta#
+   82  wget "https://www.ncbi.nlm.nih.gov/nuccore/X01714.1?report=fasta"
+   83  wget https://www.ncbi.nlm.nih.gov/nuccore/X01714.1?report=fasta
+   84  grep -v "^>" sequence.fasta | wc -1
+   85  grep -v "^>" sequence.fasta | wc -l
+   86  grep -o "A" sequence.fasta | wc -l
+   87  grep -o "G" sequence.fasta | wc -l
+   88  grep -o "C" sequence.fasta | wc -l
+   89  grep -o "T" sequence.fasta | wc -l
+   90  history > kareem1.sh
+   91  clear
+   92  cd ..
+   93  cd biocomputing
+   94  cg_count=$(grep -o "[GC]" sequence.fasta | wc -l)
+   95  total_count=$(grep -o "[ATGC]" sequence.fasta | wc -l)
+   96  gc_percentage=$(echo "scale=2;($cg_count/$total_count)*" | bc )
+   97  gc_percentage=$(echo "scale=2;( $cg_count/ $total_count)*100" | bc )
+   98  echo "GC percentage is $gc_percentage %"
+   99  mkdir kareem.fasta
+  100  rmdir kareem.fasta
+  101  touch kareem.fasta 
+  102  echo " A:395, G:413, C:422,T:381">> kareem.fasta 
+  103  git add output/kareem.fasta
+  104  sudo apt install git
+  105  git --version
+  106  git add output/kareem.fasta
+  107  git init
+  108  git add output/kareem.fasta
+  109  mkdir -p /output
+  110  sudo mkdir -p  /output
+  111  ls/
+  112  ls /
+  113  git clone https://github.com/username/repository.git
+  114  git clone https://github.com/kareemabdelgawad/HackBio-Cancer-Internship
+  115  cd repository
+  116  ls
+  117  cd HackBio-Cancer-Internship
+  118  mkdir -p output
+  119  mv /home/kareem/biocomputing/kareem.fasta /home/kareem/biocomputing/HackBio-Cancer-Internship/output/
+  120  git add output/kareem.fasta
+  121  git commit -m "nucleotide counts file"
+  122  git config --global user.name "kareem abdelgawad"
+  123  git config --global user.name "kareemabdelgawad660@gmail.com"
+  124  git commit -m "nucleotide counts file"
+  125  
+  126  git config user.email
+  127  git config user.email "kareemabdelgawad660"
+  128  git config --global --list
+  129  git config --global user.email "kareemabdelgawad660"
+  130  git commit -m "nucleotide counts file"
+  131  git push origin main
+  132  git status
+  133  git pushorigin main 
+  134  git push origin name 
+  135  git push origin main 
+  136  git remote set url origin https://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@github.com/kareemabdelgawad/HackBio-Cancer-Internship
+  137  git remote set-url origin https://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@github.com/kareemabdelgawad/HackBio-Cancer-Internship
+  138  cd output/
+  139  git push origin main
+  140  ../
+  141  ..
+  142  cd..
+  143  cd ..
+  144  history kareem.sh
+  145  history > kareem.sh
+  146  clear
+  147  cd ..
+  148  sudo apt update
+  149  sudo apt upgrade
+  150  sudo apt -y install r-base gdebi-core
+  151  lsb_release -a
+  152  cat /etc/os-release
+  153  hostnamectl
+  154  cd Downloads/
+  155  sudo gdebi rstudio-2024.04.2-764-amd64.deb
+  156  cd ..
+  157  sudo apt install libssl1.1
+  158  sudo apt install ./rstudio-2024.04.2-764-amd64.deb
+  159  sudo apt install rstudio-2024.04.2-764-amd64.deb
+  160  wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.23_amd64.deb
+  161  cd ~/Downloads
+  162  sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.23_amd64.deb
+  163  sudo gdebi rstudio-2024.04.2-764-amd64.deb
+  164  mkdir kareem_abdelgawad
+  165  rmdir kareem_abdelgawad/
+  166  mkdir kareem_abdelgawad
+  167  mkdir biocomputing && cd biocomputing
+  168  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  169  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  170  mv wildtype.fna ../kareem_abdelgawad/
+  171  rm wildtype.gbk
+  172  grep “tatatata” ../kareem_abdelgawad/wildtype.fna
+  173  grep “tatatata” ../kareem_abdelgawad/wildtype.fna > mutant_sequence.text
+  174  wget "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=X01714.1&db=nuccore&report=fasta&retmode=text" -O X01714.fasta
+  175  grep -v “^>” X01714.fasta | wc -l
+  176  grep -v "^>" X01714.fasta | wc -l
+  177  grep -o “A” X01714.fasta | wc -l
+  178  grep -o "A" X01714.fasta | wc -l
+  179  grep -o "G" X01714.fasta | wc -l
+  180  grep -o "C" X01714.fasta | wc -l
+  181  grep -o "T" X01714.fasta | wc -l
+  182  cg_count=$(grep -o "[GC]" X01714.fasta | wc -l)
+  183  total_count=$(grep -o "[ATGC]" X01714.fasta | wc -l)
+  184  gc_percentage=$(echo "scale=2;( $cg_count/ total_count)*100" | bc )
+  185  cg_count=$(grep -o "[GC]" X01714.fasta | wc -l)
+  186  total_count=$(grep -o "[ATGC]" X01714.fasta | wc -l)
+  187  gc_percentage=$(echo "scale=2;( $cg_count/ total_count)*100" | bc )
+  188  gc_percentage=$(echo "scale=2;( $cg_count/ total_count)* 100" | bc )
+  189  gc_percentage=$(echo "scale=2;( $cg_count/ $total_count)*100" | bc )
+  190  $echo "GC percentage is $gc_percentage %"
+  191  touch kareem.fasta
+  192  echo " A:395, G:413, C:422,T:381">> kareem.fasta
+  193  git status 
+  194  git add kareem.fasta 
+  195  git init
+  196  git clone https://github.com/kareemabdelgawad/HackBio-Cancer-Internship.git
+  197  cd HackBio-Cancer-Internship
+  198  mkdir /output
+  199  mkdir -p /output
+  200  cd ..
+  201  cd HackBio-Cancer-Internship/
+  202  mkdir output
+  203  mv /home/kareem/biocomputing/kareem.fasta output/
+  204  git add output/kareem.fasta
+  205  git commit -m "nucleotide counts file"
+  206  git push origin main
+  207  git remote set-url origin https://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@github.com/kareemabdelgawad/HackBio-Cancer-Internship
+  208  git push origin main
+  209  history kareem.sh
+  210  history > kareem.sh 
+  211  git status 
+  212  git add kareem.sh 
+  213  mkdir /script 
+  214  sudo mkdir /script 
+  215  mv /home/kareem/biocomputing/HackBio-Cancer-Internship/ /script/
+  216  sudo mv /home/kareem/biocomputing/HackBio-Cancer-Internship/ /script/
+  217  git add /script/
+  218  sudo rm /script/
+  219  sudo rmdir /script/
+  220  sudo rm kareem.sh 
+  221  sudo rmdir /script/
+  222  ls
+  223  mkdir script
+  224  cd..
+  225  cd ..
+  226  ls
+  227  cd ~/biocomputing/HackBio-Cancer-Internship/
+  228  ls
+  229  ls /script
+  230  sudo mv /script/HackBio-Cancer-Internship /home/kareem/biocomputing/
+  231  ls
+  232  sudo rm -r /script
+  233  sudo rmdir -r /script
+  234  sudo rm -r /script
+  235  cd ..
+  236  history > kareem.sh
+  237  cd ~
+  238  history > kareem.sh 
+  239  rm kareem.sh
+  240  cd biocomputing/
+  241  cd HackBio-Cancer-Internship/
+  242  history > karemm.sh 
+  243  rm karemm.sh 
+  244  history > kareem.sh
